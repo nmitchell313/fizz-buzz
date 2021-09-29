f577b5786c1e74bd36406630c667c0d9bda56f0a
nmitchell313 <nmitchel11@cnm.edu>
2021-09-29 12:05:17 -0600

    created fizz-buzz code.

---
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -0,0 +1,26 @@
+package edu.cnm.deepdive;
+
+public class FizzBuzz {
+
+    public String getFizzBuzz(int value) {
+        String str;
+        if (value % 3 == 0) {
+            if (value % 5 == 0) {
+                str = "FizzBuzz";
+            } else {
+                str = "Fizz";
+            }
+        } else {
+            if (value % 5 == 0) {
+                str = "Buzz";
+            } else {
+                str = String.valueOf(value);
+            }
+        }
+
+        return str;
+
+
+    }
+
+}
