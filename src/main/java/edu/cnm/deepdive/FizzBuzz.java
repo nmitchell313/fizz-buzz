39ccba02f20fc449366f4c9725279fd55a8c4abb
nmitchell313 <nmitchel11@cnm.edu>
2021-09-29 15:18:24 -0600

    JUnit tests by expected outcome.

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -2,16 +2,20 @@ package edu.cnm.deepdive;
 
 public class FizzBuzz {
 
+    static final String FIZZ_BUZZ_VALUE = "FizzBuzz";
+    static final String FIZZ_VALUE = "Fizz";
+    static final String BUZZ_VALUE = "Buzz";
+
     public String getFizzBuzz(int value) {
         String str;
         if (value % 3 == 0) {
             if (value % 5 == 0) {
-                str = "FizzBuzz";
+                str = FIZZ_BUZZ_VALUE;
             } else {
-                str = "Fizz";
+                str = FIZZ_VALUE;
             }
         } else if (value % 5 == 0) {
-            str = "Buzz";
+            str = BUZZ_VALUE;
         } else {
             str = String.valueOf(value);
         }
